5152a507ce0cf4bc7bff8c5ed4afcaf38b7e66ce
JT <certainthings@gmail.com>
2020-05-20 12:46:19 +1000

    javascript conditional solutions i

---
+++ 01-javascript/conditionals/js/exercises.js
@@ -0,0 +1,52 @@
+// # Exercises: if/else statements
+//
+// ## What number's bigger?
+//
+// Write a function named greaterNum that:
+// - takes 2 arguments, both numbers.
+// - returns whichever number is the greater (higher) number.
+// - Call that function 2 times with different number pairs, and log the output to make sure it works (e.g. "The greater number of 5 and 10 is 10.").
+
+const greaterNum = function (a, b) {
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
+};
+
+console.log(`The greater number of 5 and 10 is ${ greaterNum(5, 10) }`);
+console.log(`The greater number of 10 and 5 is ${ greaterNum(10, 5) }`);
+
+// ## The World Translator
+//
+// Write a function named helloWorld that:
+// - takes 1 argument, a language code (e.g. "es", "de", "en")
+// - returns "Hello, World" for the given language, for atleast 3 languages. It should default to returning English.
+// - Call that function for each of the supported languages and log the result to make sure it works.
+
+const helloWorld = function (code) {
+  return 'Hello World';
+};
+
+console.log( helloWorld('es') ); // Spanish
+console.log( helloWorld('de') ); // German
+console.log( helloWorld('en') ); // English
+console.log( helloWorld('ko') ); // Korean
+
+// ## The Grade Assigner
+//
+// Write a function named assignGrade that:
+// - takes 1 argument, a number score.
+// - returns a grade for the score, either "A", "B", "C", "D", or "F".
+// - Call that function for a few different scores and log the result to make sure it works.
+
+
+
+// ## The Pluralizer
+//
+// Write a function named pluralize that:
+// - takes 2 arguments, a noun and a number.
+// - returns the number and pluralized form, like "5 cats" or "1 dog".
+// - Call that function for a few different scores and log the result to make sure it works.
+// - Bonus: Make it handle a few collective nouns like "sheep" and "geese".

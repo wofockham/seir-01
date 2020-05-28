db9f3655f7930112c8491b30de5f165a8e37d282
JT <certainthings@gmail.com>
2020-05-28 13:02:55 +1000

    jquery thumbnailing

--- 04-jquery/youtube-ii/js/player.js
+++ 04-jquery/youtube-ii/js/player.js
@@ -1,10 +1,28 @@
-const links = document.querySelectorAll('a');
+const $links = $('a'); // Hungarian notation for a jQuery object
 
-for (let i = 0; i < links.length; i++) {
-  const link = links[i]; // current link
-  const href = link.getAttribute('href'); // equivalent to: link.href;
+for (let i = 0; i < $links.length; i++) {
+  const link = $links[i]; // using square brackets with jQuery you get the raw DOM node
+  const $link = $( link ); // put jQuery magic back onto the DOM node
+
+  const href = $link.attr('href'); // .attr() getter (1 argument)
   const thumbnailURL = youtube.generateThumbnailUrl(href);
-  const img = document.createElement('img'); // detached DOM node
-  img.setAttribute('src', thumbnailURL);
-  link.appendChild(img);
+
+  const $img = $('<img>'); // detached DOM node
+  $img.attr('src', thumbnailURL); // .attr() setter (2 arguments)
+
+  $link.prepend($img);
 }
+
+// $('li a').each(function () {
+//   const href = $(this).attr('href');
+//   const thumbnailURL = youtube.generateThumbnailUrl(href);
+//   const $img = $('<img>').attr('src', thumbnailURL); // chaining
+//   $(this).prepend($img);
+// });
+
+// Too clever:
+// $('li a').each(function () {
+//   $(this).prepend(
+//     $('<img>').attr('src', youtube.generateThumbnailUrl($(this).attr('href')))
+//   )
+// });

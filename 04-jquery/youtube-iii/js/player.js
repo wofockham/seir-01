e5dcd3cc7d50c324dd9c791d8696b75ad58dd8b4
JT <certainthings@gmail.com>
2020-05-28 15:01:39 +1000

    jquery youtube player

---
+++ 04-jquery/youtube-iii/js/player.js
@@ -0,0 +1,22 @@
+
+const thumbnailify = function ($link) {
+  const href = $link.attr('href'); // .attr() getter (1 argument)
+  const thumbnailURL = youtube.generateThumbnailUrl(href);
+
+  const $img = $('<img>'); // detached DOM node
+  $img.attr('src', thumbnailURL); // .attr() setter (2 arguments)
+
+  $link.prepend($img);
+
+  $link.click(function (event) {
+    event.preventDefault(); // Disable navigation to the next page.
+    const embedURL = youtube.generateEmbedUrl(href);
+    $('#player').hide().html(`<iframe width="560" height="315" src="${ embedURL }" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`).fadeIn(3000);
+  });
+};
+
+const $links = $('a'); // Hungarian notation for a jQuery object
+
+for (let i = 0; i < $links.length; i++) {
+  thumbnailify( $($links[i]) );
+}

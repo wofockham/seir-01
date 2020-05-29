47082f4617e9e96ef071e9b56e4a4be4007f5155
JT <certainthings@gmail.com>
2020-05-29 14:50:19 +1000

    stupid jquery tricks

--- 04-jquery/playground/js/playground.js
+++ 04-jquery/playground/js/playground.js
@@ -1,7 +1,25 @@
 $(document).ready(function () {
   $('#name').on('focus', function () {
-    console.log('focus');
+    $('header').addClass('active');
   }).on('blur', function () {
-    console.log('blur');
+    $('header').removeClass('active');
+  }).on('keyup', function () {
+    const input = $(this).val();
+    $('header h1').text('Welcome' + input);
+  });
+
+  const $bill = $('img'); // Cacheing
+  $(window).on('mousemove', function (event) {
+    $bill.css('opacity', event.clientY / window.innerHeight);
+  });
+
+  const randomColor = function () {
+    return Math.random() * 255; /* 0-255 */
+  };
+
+  const $body = $('body'); // Cacheing
+  $(window).on('mousemove', function () {
+    const bgColor = `rgb(${ randomColor() }, ${ randomColor() }, ${ randomColor() })`;
+    $body.css('background-color', bgColor);
   });
 });

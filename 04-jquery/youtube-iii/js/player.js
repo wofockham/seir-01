392aa48d695266e31ac6aa5f66200c750586f0f7
JT <certainthings@gmail.com>
2020-05-29 14:00:41 +1000

    document ready

--- 04-jquery/youtube-iii/js/player.js
+++ 04-jquery/youtube-iii/js/player.js
@@ -1,25 +1,26 @@
+$(document).ready(function () {
+  const thumbnailify = function ($link) {
+    const href = $link.attr('href'); // .attr() getter (1 argument)
+    const thumbnailURL = youtube.generateThumbnailUrl(href);
 
-const thumbnailify = function ($link) {
-  const href = $link.attr('href'); // .attr() getter (1 argument)
-  const thumbnailURL = youtube.generateThumbnailUrl(href);
+    const $img = $('<img>'); // detached DOM node
+    $img.attr('src', thumbnailURL); // .attr() setter (2 arguments)
 
-  const $img = $('<img>'); // detached DOM node
-  $img.attr('src', thumbnailURL); // .attr() setter (2 arguments)
+    $link.prepend($img);
 
-  $link.prepend($img);
+    $link.click(function (event) {
+      event.preventDefault(); // Disable navigation to the next page.
+      const embedURL = youtube.generateEmbedUrl(href);
+      $('#player').hide().html(`<iframe width="560" height="315" src="${ embedURL }" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`).fadeIn(3000);
+    });
+  };
 
-  $link.click(function (event) {
-    event.preventDefault(); // Disable navigation to the next page.
-    const embedURL = youtube.generateEmbedUrl(href);
-    $('#player').hide().html(`<iframe width="560" height="315" src="${ embedURL }" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`).fadeIn(3000);
-  });
-};
+  const $links = $('a'); // Hungarian notation for a jQuery object
 
-const $links = $('a'); // Hungarian notation for a jQuery object
+  for (let i = 0; i < $links.length; i++) {
+    thumbnailify( $($links[i]) );
+  }
 
-for (let i = 0; i < $links.length; i++) {
-  thumbnailify( $($links[i]) );
-}
-
-$('h2').funText(100, 'reverseRainbow');
-$('a').funText(50, 'reverseRainbow');
+  $('h2').funText(100, 'reverseRainbow');
+  $('a').funText(50, 'reverseRainbow');
+});

5feb324cad1e8825aaee4723c906ee9b0dbfcfae
JT <certainthings@gmail.com>
2020-06-09 12:45:37 +1000

    loops

--- 05-ruby/intro.rb
+++ 05-ruby/intro.rb
@@ -35,3 +35,45 @@ end
 
 # unless also has a modifier form
 puts "no 13 is less" unless 13 > 100
+
+# Loops ########################################################################
+
+i = 0 # init
+while i < 5 # conditional
+  puts "i is currently: #{ i }"
+  i += 1 # update
+end
+
+# Control-c to quit an infinite loop in Ruby
+# while true
+#   print "ZOMG"
+# end
+
+puts "-=" * 20
+
+i = 0
+until i == 5
+  puts "i is currently: #{ i }"
+  i += 1
+end
+
+# Iterators ####################################################################
+# 5.times { puts "whatever" } # one-liners are for single statements
+
+5.times do
+  puts "whatever"
+  puts "something else"
+end
+
+5.times do |j|
+  puts "j is currently: #{ j }"
+end
+
+5.downto(0) do |i|
+  puts "i is currently: #{ i }"
+end
+
+# for loops # Don't ever use these:
+for i in 0..5
+  puts i
+end

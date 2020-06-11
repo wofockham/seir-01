a9d6d997a3455e26717a0eccd247d2cb46738d0d
JT <certainthings@gmail.com>
2020-06-11 12:59:27 +1000

    sinatra calculator

---
+++ 06-sinatra/calculator/main.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pry'
+
+get '/' do
+  erb :home # embedded ruby
+end
+
+get '/faq' do
+  erb :faq
+end
+
+get '/result' do
+  x = params[:x].to_f
+  y = params[:y].to_f
+  result = case params[:operator]
+  when '+' then x + y
+  when '-' then x - y
+  when '*' then x * y
+  when '/' then x / y
+  end
+
+  "The result is #{ result }"
+end

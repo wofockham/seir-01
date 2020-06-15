d5c0060b0c5933af18484f75fadde1e351378e7f
JT <certainthings@gmail.com>
2020-06-15 15:10:58 +1000

    rails intro

---
+++ 08-rails/rails-intro/app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def hello
+    render :hello
+  end
+
+  def about
+    render :about
+  end
+
+  def faq
+    render :faq # app/views/pages/faq.html.erb
+  end
+end
---
+++ 08-rails/rails-intro/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+      # route     'controller#action'
+  root :to => 'pages#hello'
+  get '/hello' => 'pages#hello'
+  get '/about' => 'pages#about'
+  get '/faq' => 'pages#faq'
+
+  # /auto/purple => params[:color]
+  get '/auto/:color' => 'auto#color'
+  get '/auto/:hp/:torque' => 'auto#engine'
+
+  get '/calc/:x/:y/:operator' => 'calc#calculate'
+end

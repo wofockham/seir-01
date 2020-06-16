19ea1b832c59117dfcfe89a087b27d29512a5d1a
JT <certainthings@gmail.com>
2020-06-16 15:50:03 +1000

    planet CRUD

--- 08-rails/solar_system/config/routes.rb
+++ 08-rails/solar_system/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root :to => 'planets#index'
   get '/planets' => 'planets#index'
   get '/planets/new' => 'planets#new', :as => 'new_planet' # new_planet_path
   post '/planets' => 'planets#create'
-  get '/planets/:id' => 'planets#show', :as => 'planet'
+  get '/planets/:id' => 'planets#show', :as => 'planet' # planet_path
+  get '/planets/:id/edit' => 'planets#edit', :as => 'edit_planet' # edit_planet_path
+  post '/planets/:id' => 'planets#update'
+  delete '/planets/:id' => 'planets#destroy'
 end

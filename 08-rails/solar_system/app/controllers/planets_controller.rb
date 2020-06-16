cf8c1545711fb6b75c190e6852f0b3a5557606da
JT <certainthings@gmail.com>
2020-06-16 15:00:00 +1000

    planets create

--- 08-rails/solar_system/app/controllers/planets_controller.rb
+++ 08-rails/solar_system/app/controllers/planets_controller.rb
@@ -2,4 +2,16 @@ class PlanetsController < ApplicationController
   def index
     @planets = Planet.all
   end
+
+  def show
+    @planet = Planet.find params[:id]
+  end
+
+  def new
+  end
+
+  def create
+    planet = Planet.create :name => params[:name], :image => params[:image], :orbit => params[:orbit], :diameter => params[:diameter], :mass => params[:mass], :moons => params[:moons]
+    redirect_to planet_path(planet.id) # show
+  end
 end
--- 08-rails/solar_system/config/routes.rb
+++ 08-rails/solar_system/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get '/planets' => 'planets#index'
+  get '/planets/new' => 'planets#new', :as => 'new_planet' # new_planet_path
+  post '/planets' => 'planets#create'
+  get '/planets/:id' => 'planets#show', :as => 'planet'
 end

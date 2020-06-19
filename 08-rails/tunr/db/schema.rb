05bc03f92c41a86c996359564d954b7892927c2d
JT <certainthings@gmail.com>
2020-06-19 10:58:55 +1000

    admin and authorisation

--- 08-rails/tunr/db/schema.rb
+++ 08-rails/tunr/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_18_022632) do
+ActiveRecord::Schema.define(version: 2020_06_19_003328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2020_06_18_022632) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end

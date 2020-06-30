b3c5138835f2f2363ea9a2722b4a4b6077630b5a
JT <certainthings@gmail.com>
2020-06-30 12:07:04 +1000

    whatever trevor

--- 10-ajax/book-search/js/search.js
+++ 10-ajax/book-search/js/search.js
@@ -1,8 +1,7 @@
 const fetchCover = function (event) {
   event.preventDefault(); // prevent the browser from submitting the form data.
-  // find the title
   const title = document.getElementById('book_title').value;
-  // make the ajax request
+
   const xhr = new XMLHttpRequest(); // 0
   const url = `https://www.googleapis.com/books/v1/volumes?q=title:${ title }`;
   xhr.open('GET', url); // 1
@@ -12,7 +11,6 @@ const fetchCover = function (event) {
       return; // not ready yet
     }
 
-    // if we get here, the AJAX request is finished.
     const data = JSON.parse( xhr.responseText );
     const cover = data.items[0].volumeInfo.imageLinks.thumbnail;
 

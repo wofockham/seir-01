69c20e34751345c55f1be7ee9d2f4ac0be45691d
JT <certainthings@gmail.com>
2020-07-02 11:09:59 +1000

    most of the flickr search homework

--- 10-ajax/flickr-search/js/search.js
+++ 10-ajax/flickr-search/js/search.js
@@ -1,4 +1,15 @@
+// throttling -- too many requests
+
+const state = {
+  page: 1,
+  lastPageReached: false
+};
+
 const searchFlickr = function (keywords) {
+  if (state.lastPageReached) {
+    return; // exit
+  }
+
   console.log('Searching Flickr for', keywords);
 
   const flickrURL = 'https://api.flickr.com/services/rest?jsoncallback=?'; // JSONP
@@ -6,8 +17,12 @@ const searchFlickr = function (keywords) {
     method: 'flickr.photos.search', // not to be confused with method="POST"
     api_key: '2f5ac274ecfac5a455f38745704ad084',
     text: keywords, // what we are actually searching for
-    format: 'json'
+    format: 'json',
+    page: state.page++
   }).done(showImages).done(function (info) {
+    if (info.photos.pages <= info.photos.page) {
+      state.lastPageReached = true;
+    }
     console.log(info); // debugging
   });
 };
@@ -38,7 +53,11 @@ $(document).ready(function () {
   $('#search').on('submit', function (event) {
     event.preventDefault(); // disabled the form submission
     const term = $('#query').val();
+    $('#images').empty();
+    state.page = 1;
+    state.lastPageReached = false;
     searchFlickr(term);
+
   });
 
   $(window).on('scroll', function () {

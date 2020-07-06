2cbe1055b5450d116ff620027ed2b5f92e7b2a2f
JT <certainthings@gmail.com>
2020-07-06 11:06:37 +1000

    recursion in js

--- 12-comp-sci/recursion-exercises/recursion.js
+++ 12-comp-sci/recursion-exercises/recursion.js
@@ -2,12 +2,30 @@
 // You will have to figure out what parameters to include
 // All functions must use recursion
 
-function findMax(){
-    // This function returns the largest number in a given array.
+function findMax(array, largest=-Infinity){
+    if (array.length === 0) {
+      return largest;
+    }
+
+    if (array[0] > largest) {
+      largest = array[0]; // new largest value.
+    }
+
+    const rest = array.slice(1); // _.rest()
+
+    return findMax(rest, largest);
 }
 
-function factorial(){
+function factorial(n){
     // This function returns the factorial of a given number.
+    // if (n <= 1) { // base case
+    //   return 1;
+    // } else {
+    //   return n * factorial(n - 1); // recursive
+    // }
+
+    return (n <= 1) ? 1 : n * factorial(n-1);
+
 }
 
 function fibonacci(){
@@ -16,13 +34,30 @@ function fibonacci(){
     // For this function, the first two fibonacci numbers are 1 and 1
 }
 
-function coinFlips(){
+function coinFlips(tosses){
     // This function returns an array of all possible outcomes from flipping a coin N times.
     // Input type: Integer
     // For example, coinFlips(2) would return the following:
     // ["HH", "HT", "TH", "TT"]
     // H stands for Heads and T stands for tails
     // Represent the two outcomes of each flip as "H" or "T"
+
+    const combinations = [];
+
+    // helper
+    const flip = (soFar="") => {
+      console.log( "soFar", soFar, soFar.length === tosses);
+      if (soFar.length === tosses) {
+        combinations.push(soFar); // base case
+      } else {
+        flip(soFar + "H");
+        flip(soFar + "T");
+       }
+    }
+
+    flip();
+
+    return combinations;
 }
 
 function letterCombinations(){

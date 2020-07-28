93d4d6f0eb916abaa38da0fdd9a968b491eaac67
JT <certainthings@gmail.com>
2020-07-28 12:56:55 +1000

    github search basic component skellingtons and router

---
+++ 13-react/github-search/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      Home Coming Soon
+    </div>
+  );
+}
+
+export default Home;
--- 13-react/github-search/src/index.js
+++ 13-react/github-search/src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
-import App from './components/App';
+import Home from './components/Home';
+import Search from './components/Search';
+import Profile from './components/Profile';
 
-ReactDOM.render(<App />, document.body);
+// NB: Not a function, it's a variable with some JSX: Routes
+const Routes = (
+  <Router>
+    <div>
+      <Route exact path="/" component={ Home } />
+      <Route exact path="/search" component={ Search } />
+      <Route path="/details/:username" component={ Profile } />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(Routes, document.body);

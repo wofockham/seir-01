4a540796db0b448e24dbd5eafdf17f18abf1b2a2
JT <certainthings@gmail.com>
2020-08-04 14:24:34 +1000

    gatsby with data skellington

---
+++ 14-advanced/gatsby/04-data/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <h1>Site Subtitle Here</h1>
+      <div>
+        <img src="http://www.fillmurray.com/500/800" />
+      </div>
+    </Layout>
+  )
+}
